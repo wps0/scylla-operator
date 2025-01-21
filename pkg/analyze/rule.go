40f0a371c010b8b7f3c4a0ccd1b1e9577401be29
Piotr Wieczorek <53907986+wps0@users.noreply.github.com>
2025-01-21 16:30:01 +0100

    no reflections

--- pkg/analyze/rule.go
+++ pkg/analyze/rule.go
@@ -1,13 +1,9 @@
 package analyze
 
 import (
-	"errors"
-	"fmt"
 	scyllav1 "github.com/scylladb/scylla-operator/pkg/api/scylla/v1"
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/fields"
-	"reflect"
-	"strings"
+	storagev1 "k8s.io/api/storage/v1"
 )
 
 type InterfaceIter = func() interface{}
@@ -18,36 +14,9 @@ type Condition interface {
 	Kind() string
 }
 
-type ResourceCondition struct {
+type ResourceCondition[T any] struct {
 	ResKind   string
-	Condition fields.Selector
-}
-
-func (r *ResourceCondition) requirementMatches(obj interface{}, requirement fields.Requirement) bool {
-	it := GetFieldValueIterator(requirement.Field, obj)
-	val := it()
-	for val != nil && val.String() != requirement.Value {
-		val = it()
-	}
-	return val != nil
-}
-
-func (r *ResourceCondition) Evaluate(ds *DataSource) InterfaceIter {
-	return func() interface{} { return nil }
-}
-
-func (r *ResourceCondition) EvaluateOnResource(res interface{}) bool {
-	reqs := r.Condition.Requirements()
-	for _, req := range reqs {
-		if !r.requirementMatches(res, req) {
-			return false
-		}
-	}
-	return true
-}
-
-func (r *ResourceCondition) Kind() string {
-	return r.ResKind
+	Condition func(T) bool
 }
 
 type FunctionalCondition[T any] struct {
@@ -80,91 +49,24 @@ type resourceConnection struct {
 	Exists bool
 }
 
-type EqualFieldsRelation struct {
-	LhsPath string
-	RhsPath string
+type FunctionalEqualStringFieldsRelation[L, R any] struct {
+	LhsExtractor func(lhs L) []string
+	RhsExtractor func(lhs R) []string
 }
 
-func (r *EqualFieldsRelation) EvaluateOn(a interface{}, b interface{}) (bool, error) {
-	lhsIter := GetFieldValueIterator(r.LhsPath, a)
-	rhsIter := GetFieldValueIterator(r.RhsPath, b)
-	lhsAll := make([]*reflect.Value, 0)
-	for lhs := lhsIter(); lhs != nil; lhs = lhsIter() {
-		lhsAll = append(lhsAll, lhs)
-	}
-	for rhs := rhsIter(); rhs != nil; rhs = rhsIter() {
-		for _, lhs := range lhsAll {
-			if lhs.String() == rhs.String() {
-				return true, nil
+func (f *FunctionalEqualStringFieldsRelation[L, R]) EvaluateOn(a interface{}, b interface{}) (bool, error) {
+	if a != nil {
+		for _, lhs := range f.LhsExtractor(a.(L)) {
+			if b != nil {
+				for _, rhs := range f.RhsExtractor(b.(R)) {
+					if lhs == rhs {
+						return true, nil
+					}
+				}
 			}
 		}
 	}
 	return false, nil
-}
-
-type FunctionalEqualStringFieldsRelation[L, R any] struct {
-	LhsExtractor func(lhs L) string
-	RhsExtractor func(lhs R) string
-}
-
-func (f *FunctionalEqualStringFieldsRelation[L, R]) EvaluateOn(a interface{}, b interface{}) (bool, error) {
-	return f.LhsExtractor(a.(L)) == f.RhsExtractor(b.(R)), nil
-}
-
-func getFieldValueIterator(node reflect.Value, fields []string) func() *reflect.Value {
-	switch node.Kind() {
-	case reflect.Ptr:
-		return getFieldValueIterator(node.Elem(), fields)
-	}
-
-	if len(fields) == 0 {
-		called := false
-		return func() *reflect.Value {
-			if !called {
-				called = true
-				return &node
-			}
-			return nil
-		}
-	}
-
-	switch node.Kind() {
-	case reflect.Map:
-		return getFieldValueIterator(node.MapIndex(reflect.ValueOf(fields[0])), fields[1:])
-	case reflect.Struct:
-		return getFieldValueIterator(node.FieldByName(fields[0]), fields[1:])
-	case reflect.Slice | reflect.Array:
-		i := -1
-		iter := func() *reflect.Value { return nil }
-		return func() *reflect.Value {
-			val := iter()
-			for val == nil && i+1 < node.Len() {
-				i++
-				iter = getFieldValueIterator(node.Index(i), fields)
-				val = iter()
-			}
-			return val
-		}
-	case reflect.Invalid:
-		return func() *reflect.Value { return nil }
-	default:
-		panic(errors.New(fmt.Sprintf("unknown field type %s for %v", node, node)))
-	}
-	return nil
-}
-
-func GetFieldValueIterator(path string, obj interface{}) func() *reflect.Value {
-	path = strings.Map(func(ch rune) rune {
-		if ch == ' ' {
-			return -1
-		}
-		return ch
-	}, path)
-	fieldNames := strings.Split(path, ".")
-	if fieldNames[0] == "Metadata" {
-		fieldNames = fieldNames[1:]
-	}
-	return getFieldValueIterator(reflect.ValueOf(obj), fieldNames)
 }
 
 type Rule struct {
@@ -199,17 +101,17 @@ var CsiDriverMissing = Rule{
 				return storageClassXfs && conditionProgressing && conditionControllerProgressing
 			},
 		},
-		&ResourceCondition{
+		&FunctionalCondition[*v1.Pod]{
 			ResKind:   "Pod",
-			Condition: fields.Everything(),
+			Condition: func(c *v1.Pod) bool { return true },
 		},
-		&ResourceCondition{
+		&FunctionalCondition[*storagev1.StorageClass]{
 			ResKind:   "StorageClass",
-			Condition: fields.Everything(),
+			Condition: func(c *storagev1.StorageClass) bool { return true },
 		},
-		&ResourceCondition{
+		&FunctionalCondition[*storagev1.CSIDriver]{
 			ResKind:   "CSIDriver",
-			Condition: fields.Everything(),
+			Condition: func(c *storagev1.CSIDriver) bool { return true },
 		},
 	},
 	Relations: []resourceConnection{
@@ -218,26 +120,32 @@ var CsiDriverMissing = Rule{
 			Rhs:    1,
 			Exists: true,
 			Rel: &FunctionalEqualStringFieldsRelation[*scyllav1.ScyllaCluster, *v1.Pod]{
-				LhsExtractor: func(lhs *scyllav1.ScyllaCluster) string { return lhs.Name },     // LhsPath: "Metadata.Name",
-				RhsExtractor: func(rhs *v1.Pod) string { return rhs.Labels["scylla/cluster"] }, // RhsPath: "Metadata.Labels.scylla/cluster",
+				LhsExtractor: func(lhs *scyllav1.ScyllaCluster) []string { return []string{lhs.Name} },
+				RhsExtractor: func(rhs *v1.Pod) []string { return []string{rhs.Labels["scylla/cluster"]} },
 			},
 		},
 		{
 			Lhs:    0,
 			Rhs:    2,
 			Exists: true,
-			Rel: &EqualFieldsRelation{
-				LhsPath: "Spec.Datacenter.Racks.Storage.StorageClassName",
-				RhsPath: "Metadata.Name",
+			Rel: &FunctionalEqualStringFieldsRelation[*scyllav1.ScyllaCluster, *storagev1.StorageClass]{
+				LhsExtractor: func(lhs *scyllav1.ScyllaCluster) []string {
+					classes := make([]string, 0)
+					for _, rack := range lhs.Spec.Datacenter.Racks {
+						classes = append(classes, *rack.Storage.StorageClassName)
+					}
+					return classes
+				},
+				RhsExtractor: func(rhs *storagev1.StorageClass) []string { return []string{rhs.Name} },
 			},
 		},
 		{
 			Lhs:    2,
 			Rhs:    3,
 			Exists: false,
-			Rel: &EqualFieldsRelation{
-				LhsPath: "Provisioner",
-				RhsPath: "Metadata.Name",
+			Rel: &FunctionalEqualStringFieldsRelation[*storagev1.StorageClass, *storagev1.CSIDriver]{
+				LhsExtractor: func(lhs *storagev1.StorageClass) []string { return []string{lhs.Provisioner} },
+				RhsExtractor: func(rhs *storagev1.CSIDriver) []string { return []string{rhs.Name} },
 			},
 		},
 	},
